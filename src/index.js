bce339e7086aa81afa328220ff71743a598dfad7
drDramen <vvpalchin@gmail.com>
2023-05-10 15:16:46 +0300

    aw-28 feat: add phone codes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const airports = require('./data/airports.json')
+const phoneCodes = require('./data/phone-codes.json')
 const { generateFlights } = require('./utils/generateData')
 
 module.exports = () => {
   return {
     airports: airports,
     flights: generateFlights(200),
+    'phone-codes': phoneCodes
   }
 }
